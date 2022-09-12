e46ec560e5c7dde282da37373370d36a7f473926
Jesús Alberto Gómez Hernández <56267916+JesusAGH13@users.noreply.github.com>
2022-09-11 19:27:29 -0500

    Added the open of files and sites

--- Eva.py
+++ Eva.py
@@ -3,6 +3,9 @@ import speech_recognition as sr
 import pyttsx3
 import pywhatkit
 import wikipedia
+import os
+import subprocess as sub
+from dictionaries import sites, files, programs
 
 name = 'Eva'
 listenner = sr.Recognizer()
@@ -10,6 +13,7 @@ engine = pyttsx3.init()
 
 voices = engine.getProperty('voices')
 engine.setProperty('voices', voices[0].id)
+engine.setProperty('rate', 145)
 
 
 def talk(text):
@@ -32,29 +36,60 @@ def listen():
     return rec
 
 
+def playYT(rec):
+    video = rec.replace('reproduce', '')
+    video = video.replace('youtube', '')
+    talk('Reproduciendo' + video + 'youtube')
+    pywhatkit.playonyt(video)
+
+
+def buscaWiki(rec):
+    search = rec.replace('busca', '')
+    wikipedia.set_lang('es')
+    wiki = wikipedia.summary(search, 1)
+    print(search + ': ' + wiki)
+    talk(wiki)
+
+
+def abrirSitio(rec):
+    for site in sites:
+        if site in rec:
+            sub.call(
+                f'start MicrosoftEdge.exe {sites[site]}', shell=True)
+            talk(f'Abriendo {site}')
+
+
+def abrirArchivo(rec):
+    for file in files:
+        if file in rec:
+            sub.Popen([files[file]], shell=True)
+            talk(f'Abriendo {file}')
+
+
+def abrirPrograma(rec):
+    for app in programs:
+        if app in rec:
+            talk(f'Abriendo {app}')
+            os.startfile(programs[app])
+
+
 def run():
     while True:
         rec = listen()
-        if 'reproduce' in rec and 'youtube' in rec:
-            video = rec.replace('reproduce', '')
-            video = video.replace('youtube', '')
-            talk('Reproduciendo' + video + 'youtube')
-            pywhatkit.playonyt(video)
-        elif 'busca' in rec:
-            search = rec.replace('busca', '')
-            wikipedia.set_lang('es')
-            wiki = wikipedia.summary(search, 1)
-            print(search + ': ' + wiki)
-            talk(wiki)
-        elif 'alarma' in rec:
-            num = rec.replace('alarma', '')
-            num = num.strip()
-            talk('Alarma activada a las ' + num + 'horas')
-        elif 'detente' in rec or 'gracias' in rec:
+        if 'detente' in rec or 'gracias' in rec or 'adiós' in rec:
             despedida = '¡Adiós!'
             print(despedida)
             talk(despedida)
             break
+        elif 'reproduce' in rec and 'youtube' in rec:
+            playYT(rec)
+        elif 'busca' in rec:
+            buscaWiki(rec)
+        elif 'abre' in rec:
+            abrirSitio(rec)
+            abrirPrograma(rec)
+        elif 'archivo' in rec:
+            abrirArchivo(rec)
 
 
 if __name__ == '__main__':
---
+++ dictionaries.py
@@ -0,0 +1,16 @@
+sites = {
+    'google': 'google.com',
+    'cursos': 'https://www.udemy.com/home/my-courses/learning/',
+    'netflix': 'netflix.com',
+    'prime': 'https://www.primevideo.com/?_encoding=UTF8&language=es_ES',
+    'diplomas': 'https://www.coursera.org/in-progress',
+}
+
+files = {
+    'portada': 'portada.docx'
+}
+
+programs = {
+    'discord': r'ruta',
+    'whatsapp': r'ruta'
+}
